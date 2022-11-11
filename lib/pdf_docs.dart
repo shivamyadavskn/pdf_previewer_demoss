bb3a0409eafaa74cc0796f02179e7542d5b5b2dc
SHIVAM YADAV <shivamyadavskn@gmail.com>
2022-11-11 11:44:17 +0530

    pdf Docs 2

---
+++ lib/pdf_docs.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PdfDoc extends StatelessWidget {
+  const PdfDoc({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black12,
+      child:Text("hello"),
+    );
+  }
+}
