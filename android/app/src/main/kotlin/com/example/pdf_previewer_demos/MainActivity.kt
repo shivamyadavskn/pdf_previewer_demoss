19eebb92f932fb6d1ea5348146b10b0a3637aaaa
SHIVAM YADAV <shivamyadavskn@gmail.com>
2022-11-11 11:21:33 +0530

    save1

---
+++ android/app/src/main/kotlin/com/example/pdf_previewer_demos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pdf_previewer_demos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage();
+  }
+}
